20ea0a5bb1e00e54d945eddc41722d6ffaba19a5
Vlad Tănăsescu <Xen0byte@users.noreply.github.com>
2021-12-19 11:22:04 +0000

    Update Udemy Course Completer

--- udemy_complete-course.js
+++ udemy_complete-course.js
@@ -1,33 +1,34 @@
 function expandSections() {
-	var arrows = document.querySelectorAll('span.udi-angle-down')
-	var processedCounter = 0;
+    var totalSections = document.querySelectorAll("button[id*='accordion-panel-title']");
+    var expandedSectionsCounter = 0;
 
-	arrows.forEach(function (arrow) {
-		processedCounter++;
-		arrow.click();
-	});
+    totalSections.forEach(function (section) {
+        if (section.getAttribute("aria-expanded") == "false") {
+            section.click();
+            expandedSectionsCounter++;
+        }
+    });
 
-	console.log(processedCounter + " sections have been expanded");
+    console.log(expandedSectionsCounter + " sections have been expanded out of " + totalSections.length + " total sections");
 }
 
 function tickCheckboxes() {
-	var checkboxes = document.querySelectorAll('input[type="checkbox"]')
-	var processedCounter = 0;
+    var checkboxes = document.querySelectorAll("input[type='checkbox'][data-purpose='progress-toggle-button']");
+    var checkedCheckboxesCounter = 0;
 
-	checkboxes.forEach(function (checkbox) {
-		if (checkbox.checked == false) {
-				processedCounter++;
-				checkbox.click();
-			}
-		
-	});
+    checkboxes.forEach(function (checkbox) {
+        if (checkbox.checked == false) {
+            checkbox.click();
+            checkedCheckboxesCounter++;
+        }
+    });
 
-	console.log(processedCounter + " lessons out of " + checkboxes.length + " have been marked completed");
+    console.log(checkedCheckboxesCounter + " lessons have been marked completed out of " + checkboxes.length + " total lessons");
 }
 
 function completeCourse() {
-	expandSections();
-	tickCheckboxes();
+    expandSections();
+    tickCheckboxes();
 }
 
 // to be used on https://www.udemy.com/course/*/learn/lecture/*
